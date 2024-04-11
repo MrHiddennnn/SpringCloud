bb40fc14ce800ac05f376f3319aaa1a57db83503
MatCelis <91212536+MrHiddennnn@users.noreply.github.com>
2024-04-11 11:08:01 -0500

    Implementacion configuracion, dependencias y rutas en el apigateway

--- springboot-servicio-apigateway/src/main/java/com/formaciondbi/spring/app/apigateway/springbootservicioapigateway/SpringbootServicioApigatewayApplication.java
+++ springboot-servicio-apigateway/src/main/java/com/formaciondbi/spring/app/apigateway/springbootservicioapigateway/SpringbootServicioApigatewayApplication.java
@@ -2,10 +2,8 @@ package com.formaciondbi.spring.app.apigateway.springbootservicioapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class SpringbootServicioApigatewayApplication {
 
 	public static void main(String[] args) {
