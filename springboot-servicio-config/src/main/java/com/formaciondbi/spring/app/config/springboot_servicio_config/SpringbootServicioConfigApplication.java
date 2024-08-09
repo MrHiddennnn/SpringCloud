8e7c862ede2bd096f39e1678db6a8dabbca057f8
MatCelis <91212536+MrHiddennnn@users.noreply.github.com>
2024-08-09 10:56:45 -0500

    Creacion de backend de config-server

---
+++ springboot-servicio-config/src/main/java/com/formaciondbi/spring/app/config/springboot_servicio_config/SpringbootServicioConfigApplication.java
@@ -0,0 +1,13 @@
+package com.formaciondbi.spring.app.config.springboot_servicio_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioConfigApplication.class, args);
+	}
+
+}
---
+++ springboot-servicio-config/src/test/java/com/formaciondbi/spring/app/config/springboot_servicio_config/SpringbootServicioConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaciondbi.spring.app.config.springboot_servicio_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
